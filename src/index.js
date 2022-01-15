9cd12bf30e4c68f3e63c1103134588e4b49e0b7b
Santosl2c <mfilype2019@gmail.com>
2022-01-15 13:29:06 -0300

    feat: implements unsplash random image

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import { getData } from "./getData.js";
+import getRandomBackgroundImage from "./unsplash/index.js";
 
-await getData("https://www.frasesdobem.com.br/");
-await getData("https://www.frasesdobem.com.br/");
+import dotenv from "dotenv";
+dotenv.config();
+
+const background = await getRandomBackgroundImage(
+  process.env.UNSPLASH_QUERY_SEARCH,
+  process.env.UNSPLASH_API_KEY
+);
+
+await getData("https://www.frasesdobem.com.br/", background);
